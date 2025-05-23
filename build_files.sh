6c79434eb48ce935229fc445de83c8e6f434f128
Moses Ngicu <ngicumoses@gmail.com>
2025-05-23 06:33:10 +0300

    deploy on vercel

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Use Python3 and pip3 explicitly
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic --noinput
