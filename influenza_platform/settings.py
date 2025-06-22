d228af8ec8032dcd58bdb82765fb8fa31fe667a1
Moses.Ngicu <ngicumoses@gmail.com>
2025-06-22 17:51:19 +0300

    define the twitter url

--- influenza_platform/settings.py
+++ influenza_platform/settings.py
@@ -151,7 +151,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.facebook.FacebookOAuth2',
     'social_core.backends.google.GoogleOAuth2',
-    'social_core.backends.twitter.TwitterOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
     'django.contrib.auth.backends.ModelBackend',
 )
 
